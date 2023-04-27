15482e1ddb59527a57223e2edff719ad30421188
abdelrhman <abdelrhman225328@bue.edu.eg>
2023-04-27 14:10:51 +0200

    A.shenawy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String url = "jdbc:derby:Database/LAWFIRMDB;";
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+
+            // create a new table
+            String sql = "CREATE TABLE mytable (id INT PRIMARY KEY, name VARCHAR(255))";
+            stmt.executeUpdate(sql);
+
+            // insert some data into the table
+            sql = "INSERT INTO mytable (id, name) VALUES (1, 'John'), (2, 'Jane')";
+            stmt.executeUpdate(sql);
+
+            // retrieve data from the table
+            sql = "SELECT * FROM mytable";
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                System.out.println("id: " + id + ", name: " + name);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
