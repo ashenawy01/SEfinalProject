09431f75198bbfc3c6d584f703aa50d643698fac
BUEdevelopers <web.project22cs@outlook.com>
2023-04-27 14:42:01 +0200

    Moutasem

---
+++ src/entity/LawType.java
@@ -0,0 +1,11 @@
+package entity;
+
+public enum LawType {
+    Civil_Case,
+    Criminal_Case,
+    Administrative_Case,
+            Constitutional_Cases,
+    Appellate_Case,
+    Bankruptcy Case,
+    Immigration Case;
+}
